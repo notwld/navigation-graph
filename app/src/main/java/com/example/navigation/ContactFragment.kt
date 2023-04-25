e7fb4fb9afe3614445e832b543b3646f87e1b683
notwld <mwfarrukh@gmail.com>
2023-04-25 23:23:00 +0500

    fragments working

--- app/src/main/java/com/example/navigation/ContactFragment.kt
+++ app/src/main/java/com/example/navigation/ContactFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.Navigation
 
 
 class ContactFragment : Fragment() {
@@ -16,6 +19,12 @@ class ContactFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.submit).setOnClickListener{
+            Toast.makeText(activity?.applicationContext ,"Message Sent!",Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/com/example/navigation/ProfileFragment.kt
+++ app/src/main/java/com/example/navigation/ProfileFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(),View.OnClickListener {
 
-
+    private lateinit var navController:NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,6 +20,16 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.contact).setOnClickListener(this)
 
+    }
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.contact -> navController.navigate(R.id.action_profileFragment_to_contactFragment)
+        }
+    }
 
 }
